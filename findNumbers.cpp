9a6b5ae66742da895f38176e48f70849b2d772ed
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2025-10-23 21:29:21 -0400

    Add comment for Problem 1295 in findNumbers.cpp
    Added a comment indicating the problem number and purpose.

---
+++ findNumbers.cpp
@@ -0,0 +1,18 @@
+// Problem 1295 - Find Numbers with Even Number of Digits
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] >= 1000 && nums[i] <= 9999){
+                count += 1;
+            } else if (nums[i] >= 10 && nums[i] <= 99){
+                count += 1;
+            } else if (nums[i] == 100000){
+                count += 1;
+            }
+        }
+        return count;
+    }
+};
